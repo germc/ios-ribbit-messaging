9285104a360959c54e49878ce2e67572529c4725
Jason Koceja <jason@koceja.com>
2013-09-02 10:34:06 -0700

    initial commit

---
+++ EditFriendsViewController.h
@@ -0,0 +1,13 @@
+//
+//  EditFriendsViewController.h
+//  Ribbit
+//
+//  Created by Jason Koceja on 9/2/13.
+//  Copyright (c) 2013 Jason Koceja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditFriendsViewController : UITableViewController
+
+@end
