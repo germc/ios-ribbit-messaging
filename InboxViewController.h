9333831db244baba4038fdb051f317f9c2c0fedc
Jason Koceja <jason@koceja.com>
2013-09-01 17:18:46 -0700

    initial commit

---
+++ InboxViewController.h
@@ -0,0 +1,13 @@
+//
+//  InboxViewController.h
+//  Ribbit
+//
+//  Created by Jason Koceja on 9/1/13.
+//  Copyright (c) 2013 Jason Koceja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InboxViewController : UITableViewController
+
+@end
