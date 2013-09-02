bde2a19b6d7b599c276585482e64d3196c4837e5
Jason Koceja <jason@koceja.com>
2013-09-02 11:50:36 -0700

    initial commit

---
+++ FriendsViewController.h
@@ -0,0 +1,13 @@
+//
+//  FriendsViewController.h
+//  Ribbit
+//
+//  Created by Jason Koceja on 9/2/13.
+//  Copyright (c) 2013 Jason Koceja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FriendsViewController : UITableViewController
+
+@end
