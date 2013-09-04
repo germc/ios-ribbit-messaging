554eb1eca706bd3b35ab784b68b2036e13b5a142
Jason Koceja <jason@koceja.com>
2013-09-03 19:24:33 -0700

    added segue to image view and filetype icons next to senders name

--- InboxViewController.m
+++ InboxViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "InboxViewController.h"
-#import <Parse/Parse.h>
+#import "ImageViewController.h"
 
 @interface InboxViewController ()
 
@@ -31,23 +31,32 @@
 - (void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
     
+    PFQuery *query = [PFQuery queryWithClassName:@"Messages"];
+    [query whereKey:@"recipientIds" equalTo:[[PFUser currentUser] objectId]];
+    [query orderByDescending:@"createdAt"];
     
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        if(error){
+            NSLog(@"Error: %@ %@", error, [error userInfo]);
+        }else{
+            // we found messages
+            self.messages = objects;
+            [self.tableView reloadData];
+            NSLog(@"Retrieved %d messages", [self.messages count]);
+        }
+    }];
 }
 
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-#warning Potentially incomplete method implementation.
-    // Return the number of sections.
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-#warning Incomplete method implementation.
-    // Return the number of rows in the section.
-    return 0;
+    return [self.messages count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -55,7 +64,17 @@
     static NSString *CellIdentifier = @"Cell";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
     
-    // Configure the cell...
+    PFObject *message = [self.messages objectAtIndex:indexPath.row];
+    cell.textLabel.text = [message objectForKey:@"senderName"];
+    
+    NSString *fileType = [message objectForKey:@"fileType"];
+    if ([fileType isEqualToString:@"image"]){
+        // if image is an image, show image icon
+        cell.imageView.image = [UIImage imageNamed:@"icon_image"];
+    }else{
+        // show video icon
+        cell.imageView.image = [UIImage imageNamed:@"icon_video"];
+    }
     
     return cell;
 }
@@ -64,13 +83,16 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    // Navigation logic may go here. Create and push another view controller.
-    /*
-     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
-     // ...
-     // Pass the selected object to the new view controller.
-     [self.navigationController pushViewController:detailViewController animated:YES];
-     */
+    self.selectedMessage = [self.messages objectAtIndex:indexPath.row];
+    
+    NSString *fileType = [self.selectedMessage objectForKey:@"fileType"];
+    if ([fileType isEqualToString:@"image"]){
+        // if image
+        [self performSegueWithIdentifier:@"showImage" sender:self];
+    }else{
+        
+    }
+
 }
 
 - (IBAction)logout:(id)sender {
@@ -81,7 +103,19 @@
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
     if([segue.identifier isEqualToString:@"showLogin"]){
         [segue.destinationViewController setHidesBottomBarWhenPushed:YES];
+    }else if([segue.identifier isEqualToString:@"showImage"]){
+        [segue.destinationViewController setHidesBottomBarWhenPushed:YES];
+        ImageViewController *imageViewController = (ImageViewController *)segue.destinationViewController;
+        imageViewController.message = self.selectedMessage;
     }
 }
 
 @end
+
+
+
+
+
+
+
+
