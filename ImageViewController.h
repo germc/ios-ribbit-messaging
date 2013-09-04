946e3a87b58b10bc2007482f5f26deab2fde5d69
Jason Koceja <jason@koceja.com>
2013-09-03 19:21:23 -0700

    initial commit

---
+++ ImageViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImageViewController.h
+//  Ribbit
+//
+//  Created by Jason Koceja on 9/3/13.
+//  Copyright (c) 2013 Jason Koceja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageViewController : UIViewController
+
+@end
